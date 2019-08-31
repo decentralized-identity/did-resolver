3f1c28c2f5a2f5e1cd29eb305101f697ca5a56fd
Pelle Braendgaard <pelle@stakeventures.com>
2019-08-31 21:07:18 +0200

    support full DID URL

--- src/__tests__/resolver-test.ts
+++ src/__tests__/resolver-test.ts
@@ -21,13 +21,15 @@ describe('resolver', () => {
         method: 'uport',
         id: '2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
         did: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
+        didUrl: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX'
       })
       expect(
         parse('did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX/some/path'),
       ).toEqual({
         method: 'uport',
         id: '2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
-        did: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX/some/path',
+        did: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
+        didUrl: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX/some/path',
         path: '/some/path',
       })
       expect(
@@ -35,7 +37,8 @@ describe('resolver', () => {
       ).toEqual({
         method: 'uport',
         id: '2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
-        did: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX#fragment=123',
+        did: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
+        didUrl: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX#fragment=123',
         fragment: 'fragment=123',
       })
       expect(
@@ -45,8 +48,8 @@ describe('resolver', () => {
       ).toEqual({
         method: 'uport',
         id: '2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
-        did:
-          'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX/some/path#fragment=123',
+        did: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX',
+        didUrl: 'did:uport:2nQtiQG6Cgm1GYTBaaKAgr76uY7iSexUkqX/some/path#fragment=123',
         path: '/some/path',
         fragment: 'fragment=123',
       })
@@ -56,7 +59,65 @@ describe('resolver', () => {
         method: 'nacl',
         id: 'Md8JiMIwsapml_FtQ2ngnGftNP5UmVCAUuhnLyAsPxI',
         did: 'did:nacl:Md8JiMIwsapml_FtQ2ngnGftNP5UmVCAUuhnLyAsPxI',
+        didUrl: 'did:nacl:Md8JiMIwsapml_FtQ2ngnGftNP5UmVCAUuhnLyAsPxI',
       })
+      expect(
+        parse('did:example:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high')
+      ).toEqual({
+        method: 'example',
+        id: '21tDAKCERh95uGgKbJNHYp',
+        did: 'did:example:21tDAKCERh95uGgKbJNHYp',
+        didUrl: 'did:example:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high',
+        params: {
+          service: 'agent',
+          'foo:bar': 'high'
+        }        
+      })
+      expect(
+        parse('did:example:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high?foo=bar')
+      ).toEqual({
+        method: 'example',
+        id: '21tDAKCERh95uGgKbJNHYp',
+        didUrl: 'did:example:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high?foo=bar',
+        did: 'did:example:21tDAKCERh95uGgKbJNHYp',
+        query: 'foo=bar',
+        params: {
+          service: 'agent',
+          'foo:bar': 'high'
+        }        
+      })
+      expect(
+        parse('did:example:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high/some/path?foo=bar#key1')
+      ).toEqual({
+        method: 'example',
+        id: '21tDAKCERh95uGgKbJNHYp',
+        didUrl: 'did:example:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high/some/path?foo=bar#key1',
+        did: 'did:example:21tDAKCERh95uGgKbJNHYp',
+        query: 'foo=bar',
+        path: '/some/path',
+        fragment: 'key1',
+        params: {
+          service: 'agent',
+          'foo:bar': 'high'
+        }        
+      })
+
+      expect(
+        parse('did:example:test:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high/some/path?foo=bar#key1')
+      ).toEqual({
+        method: 'example',
+        id: 'test:21tDAKCERh95uGgKbJNHYp',
+        didUrl: 'did:example:test:21tDAKCERh95uGgKbJNHYp;service=agent;foo:bar=high/some/path?foo=bar#key1',
+        did: 'did:example:test:21tDAKCERh95uGgKbJNHYp',
+        query: 'foo=bar',
+        path: '/some/path',
+        fragment: 'key1',
+        params: {
+          service: 'agent',
+          'foo:bar': 'high'
+        }        
+      })
+
     })
 
     it('fails if non compliant', () => {
