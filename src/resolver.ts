dc4f80dd81a58a7dcd76e15d0811f8741fe1e121
Sergey Ukustov <sergey@ukstv.me>
2020-09-26 19:21:08 +0400

    Actually, "authentication" relationship in did document is either DID URL or public key, no special Authenticator needed

--- src/resolver.ts
+++ src/resolver.ts
@@ -16,7 +16,7 @@ export interface DIDDocument {
   '@context': 'https://w3id.org/did/v1'
   id: string
   publicKey: PublicKey[]
-  authentication?: (string | Authentication)[]
+  authentication?: (string | PublicKey)[]
   uportProfile?: any
   service?: ServiceEndpoint[]
   created?: string
@@ -41,11 +41,6 @@ export interface PublicKey {
   publicKeyBase58?: string
   publicKeyHex?: string
   publicKeyPem?: string
-}
-
-export interface Authentication {
-  type: string
-  publicKey: string
 }
 
 export interface LinkedDataProof {
